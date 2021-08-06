185c6ac3ff0b110837ccbd21508930511ae0aeb6
alea najla <alea.syukur@binus.ac.id>
2021-08-06 23:34:08 +0700

    init commit

---
+++ dummy - Copy/src/pages/learning/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const learning = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Learning Screen</Text>
+        </View>
+    )
+}
+
+export default learning
+
+const styles = StyleSheet.create({})

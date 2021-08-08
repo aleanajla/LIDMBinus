f0d5e52ecab2d706ad1a656a9f4adb0fe0f366a0
alea najla <alea.syukur@binus.ac.id>
2021-08-08 17:31:18 +0700

    Update index.js

--- dummy/src/pages/home/index.js
+++ dummy/src/pages/home/index.js
@@ -8,14 +8,15 @@ const Home = () => {
         <View style={styles.pages}>
             <View style = {styles.header}>
                 <View style = {styles.welcome}>
-                    <Text>Welcome back,</Text>
-                    <Text style = {{paddingLeft: 10}}>Andrew Willy</Text>
+                    <Text style = {{fontSize: 14, color : '#28527A'}}>Welcome back,</Text>
+                    <Text style = {{fontSize: 20,fontWeight: 'bold', color: WARNA_SEKUNDER}}>Andrew Willy</Text>
                 </View>
                 <View style = {styles.imgHeader}>
                     <Bell1 styles = {styles.bell}/>
                     <User styles = {styles.user}/>
                 </View>
             </View>
+            <View style = {[styles.garis, styles.shadow]}/>
         </View>
     )
 }
@@ -26,16 +27,34 @@ const styles = StyleSheet.create({
     },
     pages : {
         //flex : 1,
-        backgroundColor : '#F6F5F5'
+        backgroundColor : '#F6F5F5',
+        fontFamily: 'Open-Sans'
     },
     header :{
-        flexDirection : 'row'
+        flexDirection : 'row',
+        paddingTop: 15,
+        backgroundColor: '#F6F5F5'
     },
     bell :{
         // justifyContent: 'flex-End'
+        
     },
     imgHeader :{
-        flexDirection : 'row'
+        flexDirection : 'row',
+        left :120,
+        justifyContent : 'space-between',
+        flex: 0.4
+    },
+    garis:{
+        paddingTop: 8,
+        borderBottomColor: '#28527A',
+        borderBottomWidth: 1,
+    },
+    shadow :{
+        shadowColor: '#000000',
+        shadowOffset: {width: 4, height: 4},
+        shadowOpacity: 10,
+        shadowRadius: 10,
     }
 })
 

0521afaace001cb805b0c12398af5f6f005e4fcb
anidadpr17 <anidadpr17@gmail.com>
2021-08-18 02:22:44 +0700

    voucher code + details book

--- dummy/src/pages/myRewards/index.js
+++ dummy/src/pages/myRewards/index.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Platform, Dimensions, FlatList} from 'react-native'
 import { WARNA_SEKUNDER, WARNA_UTAMA } from '../../utils/constants'
 import { useNavigation } from '@react-navigation/core'
 import { Profile } from '../../pages/index.js'
+import Modal from 'react-native-modal'
+
+const windowsWidth = Dimensions.get('window').width;
+const windowsHeight = Dimensions.get('window').height;
 
 const myRewards = () => {
+    const [modalVisible, setModalVisible] = useState(false);
     const navigation = useNavigation();
     return(
+        <ScrollView>
+        <Modal
+            animationType="slide" 
+            transparent={true} 
+            visible={modalVisible}
+            hasBackdrop={true} 
+            backdropOpacity={0.5}
+            onRequestClose={() => {
+                setModalVisible(!modalVisible)}}
+            >
+                <View styles={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <TouchableOpacity onPress={()=> setModalVisible(!modalVisible)}>
+                            <View style={styles.closeBtn}>
+                                <Image source = {require('../../assets/icons/closeBlack.png')}/>
+                            </View>
+                        </TouchableOpacity>
+                        <Text style={styles.text4}><Text>Voucher Code</Text></Text>
+                    <TouchableOpacity
+                        onPress={()=> setModalVisible(!modalVisible)}>
+                        <View style={styles.voucher}>
+                            <Text style={styles.closeTxt}>ABCSH1782</Text>
+                            <Image source = {require('../../assets/icons/copy.png')}/>
+                        </View>
+                    </TouchableOpacity>
+                    </View> 
+                </View>
+        </Modal>
         <View style= {styles.outer}>
             <View style= {styles.header}>  
                 <TouchableOpacity
@@ -24,9 +57,11 @@ const myRewards = () => {
                         <Text style={styles.details}>eVoucher Gofood 25k</Text>
                         <Text style={styles.untillDate}>Valid untill 29 September 2021</Text>
                     </View>
-                    <View style = {styles.btnUse}>
-                        <Text style= {styles.textUse}>Use</Text>
-                    </View>
+                    <TouchableOpacity onPress={()=> setModalVisible(true)}>
+                        <View style = {styles.btnUse}>
+                            <Text style= {styles.textUse}>Use</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
 
                 <View style={styles.eachBox}>
@@ -36,9 +71,11 @@ const myRewards = () => {
                         <Text style={styles.details}>eVoucher Gofood 50k</Text>
                         <Text style={styles.untillDate}>Valid untill 25 September 2021</Text>
                     </View>
+                    <TouchableOpacity onPress={()=> setModalVisible(true)}>
                     <View style = {styles.btnUse}>
                         <Text style= {styles.textUse}>Use</Text>
                     </View>
+                    </TouchableOpacity>
                 </View>
 
                 <View style={styles.eachBox}>
@@ -48,12 +85,15 @@ const myRewards = () => {
                         <Text style={styles.details}>eVoucher Udemy 100k</Text>
                         <Text style={styles.untillDate}>Valid untill 20 August 2021</Text>
                     </View>
+                    <TouchableOpacity onPress={()=> setModalVisible(true)}>
                     <View style = {styles.btnUse}>
                         <Text style= {styles.textUse}>Use</Text>
                     </View>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
+        </ScrollView>
     )
 }
 
@@ -72,7 +112,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFFFFF',
         borderRadius:10,
         width:334,
-        height:98
+        height:98,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        elevation:5,
     },
     boxContent:{
         margin:10,
@@ -113,5 +160,67 @@ const styles = StyleSheet.create({
     textUse:{
         color:'#FFFFFF',
         textAlign:'center'
+    },
+    centeredView :{
+        flex: 1,
+        justifyContent: "center",
+        width: windowsWidth,
+        height: windowsWidth,
+        alignItems: "center",
+    },
+    modalView: {
+        // width : 300,
+        // height :190,
+        paddingTop:20,
+        // paddingRight:35,
+        // paddingLeft:35,
+        paddingBottom:35,
+        margin:40,
+        backgroundColor: "white",
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        elevation:5,
+    },
+    text4:{
+        fontSize : 22,
+        color : '#022E57',
+        fontWeight : 'bold',
+        textAlign : 'center'
+    },
+    voucher:{
+        backgroundColor : '#F6F5F5',
+        borderRadius : 10,
+        justifyContent : 'space-between',
+        alignItems : 'center',
+        paddingLeft : 22,
+        width:235,
+        height:36,
+        marginTop : 13,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        elevation:5,
+        flexDirection : 'row',
+        paddingRight : 18,
+        marginTop : 25
+    },
+    closeTxt:{
+        color : '#28527A',
+        fontSize : 22,
+    },
+    closeBtn:{
+        alignItems : 'flex-end',
+        width : 235,
+        marginBottom : 10
     }
 })

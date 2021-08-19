90de7095c891bb4191d7b429db68d99ae2361291
keenanario <57608848+keenanario@users.noreply.github.com>
2021-08-19 10:48:31 +0700

    OpenURL
    for opening URL

--- dummy/src/components/ListView/index.js
+++ dummy/src/components/ListView/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+
+import {View, FlatList, StyleSheet, ScrollView} from 'react-native';
 import CustomRow from '../ListViewItem';
 
-
-
 const CustomListView = ({itemList}) => (
+    <ScrollView>
     <View style={{flex: 1}}>
-        <FlatList
-            data={itemList}
-            renderItem={({ item }) => <CustomRow
-                title={item.title}
-                description={item.description}
-                image_url={item.image_url}
-            />}
-        />
+            <FlatList
+                data={itemList}
+                renderItem={({ item }) => <CustomRow
+                    title={item.title}
+                    description={item.description}
+                    image_url={item.image_url}
+                />}
+            />
     </View>
+    </ScrollView>
 );
 
 export default CustomListView;
--- dummy/src/pages/schedule/index.js
+++ dummy/src/pages/schedule/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { useCallback } from 'react';
+import { StyleSheet, Text, View,Linking, Alert, Button } from 'react-native'
+
+const webURL = 'https://google.com';
+
+const OpenURLButton = ({ url, children}) => {
+    const handlePress = useCallback(async() => {
+        const supported = await Linking.canOpenURL(url);
+        if(supported){
+            await Linking.openURL(url);
+        }else{
+            Alert.alert('URL Error');
+        }
+    }, [url]);
+
+    return <Button title={children} onPress={handlePress}/>
+}
 
 const Schedule = () => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Schedule Screen</Text>
+        <View style={{ flex: 1}}>
+            <View>
+                <OpenURLButton url={webURL}>Open Supported URL</OpenURLButton>
+            </View>
         </View>
     )
 }
 
-export default Schedule;
+export default Schedule; 
 
 const styles = StyleSheet.create({})

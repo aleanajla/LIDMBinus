b8c39b1f11f1c491d0ee14ba5e245ebd5c4c5120
keenanario <57608848+keenanario@users.noreply.github.com>
2021-08-24 15:22:32 +0700

    implement search function

--- dummy/src/pages/perpus/index.js
+++ dummy/src/pages/perpus/index.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import CustomListView from '../../components/ListView'
 import { newsData } from '../../assets/jsonData/newsData'
+import { useState } from 'react/cjs/react.development'
 
 const Perpus = ({navigation}) => {
+    const [search, setSearch] = useState('');
+    const [filteredDataSource, setFilteredDataSource] = useState(newsData);
+    const [masterDataSource, setMasterDataSource] = useState(newsData);
+
+    const searchFilterFunction = (text) => {
+        if(text){
+            const newData = masterDataSource.filter(
+                function (item) {
+                    const itemData = item.title ? item.title.toUpperCase(): ''.toUpperCase();
+                    const textData = text.toUpperCase();
+                    return itemData.indexOf(textData)>-1;
+                }
+            );
+            setFilteredDataSource(newData);
+            setSearch(text);
+        }else{
+            setFilteredDataSource(masterDataSource);
+            setSearch(text);
+        }
+    }
+
     return (
         <View style={{ flex: 1}}>
+            <TextInput
+                style = {{height: 40, borderWidth: 1, paddingLeft: 20, margin:5, borderColor:"#000", backgroundColor:"#FFF"}}
+                onChangeText={(text) => searchFilterFunction(text)}
+                value={search}
+                underlineColorAndroid="transparent"
+                placeholder="Search Here"
+            />
             <CustomListView
-                itemList={newsData}
+                itemList={filteredDataSource}
                 navigation={navigation}
             />
         </View>
